29b11ec3ca5d72fac65d96fff9144e7a1c40c4c6
Vitor Dallanol <125999178+Crautor@users.noreply.github.com>
2024-05-03 09:30:54 -0300

    fix: change type from description on news

--- schemaTypes/news.js
+++ schemaTypes/news.js
@@ -1,28 +1,27 @@
 export default {
-    name: 'news',
-    type: 'document',
-    title: 'Noticias',
-    fields: [
-      {
-        name: 'title',
-        type: 'string',
-        title: 'Titulo Da Noticia',
-      },
-      {
-        name: 'subtitle',
-        type: 'string',
-        title: 'Sub Titulo Da Noticia',
-      },
-      {
-        name: 'descricao',
-        type: 'string',
-        title: 'Descrição Da Noticia',
-      },
-      {
-        name: 'image',
-        type: 'image',
-        title: 'Imagem da noticia',
-      },
-    ],
-  }
-  
+  name: 'news',
+  type: 'document',
+  title: 'Noticias',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Titulo Da Noticia',
+    },
+    {
+      name: 'subtitle',
+      type: 'string',
+      title: 'Sub Titulo Da Noticia',
+    },
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Descrição Da Noticia',
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Imagem da noticia',
+    },
+  ],
+}
