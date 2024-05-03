1c8b68a344152811632006a7e29381951e5da8cc
Vitor Dallanol <125999178+Crautor@users.noreply.github.com>
2024-05-03 08:55:16 -0300

    feat: create the carousel Schema

---
+++ schemaTypes/carousel.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'carousel',
+  type: 'document',
+  title: 'Carrosel',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Titulo Da Imagem',
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Imagem Carrosel',
+    },
+  ],
+}
--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,6 +1,3 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import carousel from './carousel'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [carousel]
